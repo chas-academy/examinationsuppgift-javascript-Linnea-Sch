3ce83e4446bd413b60bce031613fe6dfa094ff59
linnea.schogarne@gmail.com <124348893+Linnea-Sch@users.noreply.github.com>
2026-03-24 16:15:16 +0100

    Lite cleanup

--- src/script.js
+++ src/script.js
@@ -4,15 +4,20 @@ Antagningsprov Javascript Chas Academy
 Budget- och utgiftskoll
 */
 
+
+
 const html_description_input = document.getElementById("desc");
 const html_amount_input = document.getElementById("amount")
 
 const html_income_button = document.getElementById("incomeBtn");
 const html_expense_button = document.getElementById("expenseBtn");
+
 const html_income_list = document.getElementById("incomeList");
 const html_expense_list = document.getElementById("expenseList");
+
 const html_balance = document.getElementById("balance");
 
+// Saldot som vi ändrar internt i koden och ej ett element i html:en
 let balance = 0;
 
 function button_click(income_or_expense){
@@ -21,11 +26,7 @@ function button_click(income_or_expense){
     amount = html_amount_input.value;
 
     if (description == "" || amount == "" || isNaN(amount)){
-        // felhantering, som en fin text som dyker upp eller liknande
         console.log("Input-fälten är felaktigt ifyllda")
-        console.log(description)
-        console.log(amount)
-        console.log(isNaN(amount))
     }else{
         // Skapar ett nytt html-element med rätt text
         const new_element = document.createElement('li');
@@ -34,22 +35,17 @@ function button_click(income_or_expense){
         // lägger in det nya html-elementet i rätt lista 
         if (income_or_expense == "Inkomst"){
             html_income_list.appendChild(new_element);
-            console.log("Inkomst")
-            console.log(description)
-            console.log(amount)
             UpdateSaldo(income_or_expense, amount)
         }
         else if(income_or_expense == "Utgift"){
             html_expense_list.appendChild(new_element);
-            console.log("Utgift");
-            console.log(description);
-            console.log(amount);
             UpdateSaldo(income_or_expense, amount);
         }
     }
 }
 
 function UpdateSaldo(income_or_expense, amount){
+    // Uppdaterar det inre saldot i koden och sedan skickar vidare till funktionen updatesaldohtml ifall allt går som det ska
     if (income_or_expense == "Inkomst"){
         balance += Number(amount);   
         UpdateSaldoHtml();
@@ -62,15 +58,17 @@ function UpdateSaldo(income_or_expense, amount){
 function UpdateSaldoHtml(){
     html_balance.innerHTML = "";
     html_balance.innerHTML = balance;
-    console.log(balance)
 
+    // Hit kommer vi bara om allting gått rätt
     UX_reset();
 }
 
 function UX_reset(){
+    // Vi tömmer värdena i input-fälten
     html_description_input.value = "";
     html_amount_input.value = "";
 }
+
 html_income_button.addEventListener("click", () => {
     button_click("Inkomst")
 })
